479249ec023eb70bf24e460607a1f1438132f27d
Jinminzuo <110321926+Jinminzuo@users.noreply.github.com>
2025-09-25 11:36:45 +0800

    feat
    commit

--- main.go
+++ main.go
@@ -1,18 +1,12 @@
 package main
 
-import (
-	"log"
-	"net/http"
-	"wxcloudrun-golang/service"
-)
-
 func main() {
 
-	http.HandleFunc("/", service.HelloWorldHandler)
-	http.HandleFunc("/tags", service.TagsHandler)
-	http.HandleFunc("/send", service.SendHandler)
-	http.HandleFunc("/tagforuser", service.TagUsersAllHandler)
-	http.HandleFunc("/send_template_to_users", service.SendTemplateToUsersHandler)
+	//http.HandleFunc("/", service.HelloWorldHandler)
+	//http.HandleFunc("/tags", service.TagsHandler)
+	//http.HandleFunc("/send", service.SendHandler)
+	//http.HandleFunc("/tagforuser", service.TagUsersAllHandler)
+	//http.HandleFunc("/send_template_to_users", service.SendTemplateToUsersHandler)
 
-	log.Fatal(http.ListenAndServe(":80", nil))
+	//log.Fatal(http.ListenAndServe(":80", nil))
 }
